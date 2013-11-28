6caa161cbdf22e8a325882f89aaf3b0c8f620c81
tinawang2802 <tinawang2802@e1a0bdcb-e5fc-66f3-3635-27986208f306>
2013-11-28 01:16:22 +0000

    Parsing each web page without unecessary tags

--- src/Parser.py
+++ src/Parser.py
@@ -1,5 +1,45 @@
+#import sys
+#import glob
+#import errno
+from bs4 import BeautifulSoup, Comment, Doctype
+import os
 
 
+class Parser:
+	def parse(self, path):
+        #create a list which will contain the documents
+#        collection = list()
+        #Process the input file to extract documents
+		for filename in os.listdir(path):
+			input_file = open(path + filename)
+			soup = BeautifulSoup(input_file)
+			blacklist = ['script','meta', 'link', 'style', 'a','meta','button','img','iframe','br']
+			# Delete DOCTYPE declaration
+   			#items = [item for item in soup.contents if isinstance(item, Doctype)]
+    			#del items
+    		for tag in soup.findAll(True):
+			if tag.name.lower() in blacklist:
+				tag.extract()
+			for attribute in ["accesskey", "border", "bordercolor", "cellpadding", "cellspacing",
+				"width", "height", "colspan", "valign", "type", "align", "action", "style"]:
+				del tag[attribute]
+			if soup:
+				continue
+			# scripts can be executed from comments in some cases
+			comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+			for comment in comments:
+				comment.extract()
+            	print soup.prettify()
+		print '********************************************'
+#		return collection
+
+# main function
+if __name__ == '__main__':
+    collection = list()
+    parser = Parser()
+    path = "/home/tina/projects/test/"
+    parser.parse(path)
+'''
 class Parser:
     def parse(self, path):
         input_file = open(path).read()
@@ -20,3 +60,4 @@ class Parser:
             # meaning the whole collection has been processed
                 break
         return collection
+'''
