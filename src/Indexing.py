855b9d92822c9f573da210f972a5b0ebe756ddb4
tinawang2802 <tinawang2802@e1a0bdcb-e5fc-66f3-3635-27986208f306>
2013-11-29 02:59:32 +0000

    change title result set to string

--- src/Indexing.py
+++ src/Indexing.py
@@ -7,9 +7,9 @@ from bs4 import BeautifulSoup
 class Indexing:
 
     def __init__(self):
-        self.schema = Schema(title=TEXT(field_boost=5.0),
+        self.schema = Schema(title=TEXT(stored=True, field_boost=5.0),
                         th=TEXT(field_boost=4.0),
-                        h1=TEXT(field_boost=4.0),
+                        h1=TEXT(stored=True, field_boost=4.0),
                         h2=TEXT(field_boost=3.0),
                         p=TEXT(field_boost=3.0),
                         blockquote=TEXT(field_boost=3.0),
@@ -34,46 +34,82 @@ class Indexing:
         for doc in collection:
 	    # Parse document with Beautiful Soup to remove unwanted tags.
             soup = BeautifulSoup(str(doc))
-
 	    if soup.title:
-		title = soup.title.findAll(text=True)
+	        title = unicode(soup.title.findAll(text=True))
+                #titles = ""
+                #for t in title:
+                    #titles += " " + str(t.contents[0])
+            else:
+                title = None
             if soup.th:
 		th = soup.th.findAll(text=True)
+            else:
+                th = None
             if soup.h1:
 		h1 = soup.h1.findAll(text=True)
+            else:
+                h1 = None
             if soup.h2:
 		h2 = soup.h2.findAll(text=True)
-	    if soup.h3:       
+            else:
+                h2 = None
+            if soup.h3:
 		h3 = soup.h3.findAll(text=True)
-		s = ""
+		h3s = ""
 		for h in h3:
-			s += " " + str(h.contents[0])
-		print s
-	    if soup.h4:
+		    #h3s += " " + str(h.contents[0])
+		    h3s += " " + unicode(h)
+		print h3s
+            else:
+                h3 = None
+            if soup.h4:
 		h4 = soup.h4.findAll(text=True)
-	    if soup.h5:
+            else:
+                h4 = None
+            if soup.h5:
 		h5 = soup.h5.findAll(text=True)
+            else:
+                h5 = None
 	    if soup.h6:
 		h6 = soup.h6.findAll(text=True)
+            else:
+                h6 = None
 	    if soup.p:
 		p = soup.p.findAll(text=True)
-	    if soup.blockquote:
+            else:
+                p = None
+            if soup.blockquote:
 		blockquote = soup.blockquote.findAll(text=True)
-	    if soup.td:
+            else:
+                blockquote = None
+            if soup.td:
 		td = soup.td.findAll(text=True)
-#	    li = soup.li.findAll(text=True)
-#	    label = soup.label.findAll(text=True)
-#	    div = soup.div.findAll(text=True)
-#	    section = soup.section.findAll(text=True)
+            else:
+                td = None
+            if soup.li:
+	         li = soup.li.findAll(text=True)
+            else:
+                li = None
+            if soup.label:
+	         label = soup.label.findAll(text=True)
+            else:
+                label = None
+            if soup.div:
+	         div = soup.div.findAll(text=True)
+            else:
+                div = None
+            if soup.section:
+	         section = soup.section.findAll(text=True)
+            else:
+                section = None
 
+            writer.add_document(title=title, th=th, h1=h1, h2=h2, h3=h3s, h4=h4,
+                                h5=h5, h6=h6, p=p, blockquote=blockquote,
+                                td=td, li=li, label=label, div=div,
+                                section=section)
 
-#            writer.add_document(title=title, th=th, h1=h1, h2=h2, h3=s, h4=h4,
-#                                h5=h5, h6=h6, p=p, blockquote=blockquote,
-#                                td=td, li=li, label=label, div=div,
-#                                section=section)
+            print "finishing add_document"
 
-#            print "finishing add_document"
-
-#        writer.commit()
+        writer.commit()
 
 
